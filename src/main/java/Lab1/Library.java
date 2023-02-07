c9f7a49ef5dbf14acaa68e86ded97627dbfe5b5d
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2023-02-07 21:56:54 +0800

    Modified Library.java

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -3,6 +3,8 @@ package Lab1;
 public class Library {
 	
 	/* Add this function */
+	//	init commit already contain the final version of Library.java
+	//	this line is used as a file change for committing to git
 	public static void main(String arg[]) {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
